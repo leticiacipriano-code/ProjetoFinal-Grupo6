64d6fb7f689b2c6d82205aac447adaeb26f26683
Gerson Chadi Junior <gersonchadijunior@gmail.com>
2026-04-14 15:43:35 -0300

    Setup feito. Dockerfile criado, docker-compose atualizado. Todos os containeres criado com excessao do Airflow. profiles.yml para dbt criado.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import logging
+from bronze.ingest import run_ingestion
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("Hello from projetofinal-grupo6!")
+    logger.info("Iniciando Pipeline Glow & Co.")
+
+    # 1. Bronze Layer (EL)
+    logger.info("Etapa 1: Ingestão de dados (Bronze)")
+    run_ingestion()
+
+    # 2. Quality Layer (GX)
+
+    logger.info("Pipeline finalizado com sucesso !")
 
 
 if __name__ == "__main__":
